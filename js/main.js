eee88a4b8fa6fb59f5159dd6f1f80a25019904f1
Yasnovskiy <andrey5.1997@mail.ru>
2021-09-27 16:39:11 +0500

    comit(2): fixed function and added description

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-function getRandomNumbarTheValue(min, max) {
+function getRandomNumbarValue(min, max) {
+  // Округление в большую сторону, число не сможет стать меньше минимального
+  min = Math.ceil(min);
+  // Округление в меньшую сторону, значит число тоже будет включаться в рандомное
+  max = Math.floor(max);
 
-  if (min <= max) {
-    const randomNumbar = Math.random() * (max - min);
+  // Проверка на отрицительное значение
+  if (min < 0 || max < 0) {
+    return 'Одно или два значения отрицательные, это недопустимый формат!';
+  }
 
-    return Math.ceil(randomNumbar);
-  } else {
+  // Проверки минимальное значение больше или равное максимальному
+  if (min >= max) {
     return 'Минимальное значение не может быть больше Максимального! Может вы перепутили значения. Обратите на это внимание';
   }
+
+  // И если мы прошли все проверки, будем получать рандомное число из min и max значения
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomNumbarTheValue(20, 1740);
+getRandomNumbarValue(0, 1);
 
-function checkStringLength(lengthString) {
-  // 140 из технического задания
-  return lengthString <= 140;
+function checkLengthString(lengthString, validlength) {
+  return lengthString <= validlength;
 }
 
-checkStringLength(20);
+checkLengthString(20, 140);
