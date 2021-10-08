f573875a004bb4402bdfc36afa2dc317e690a340
Yasnovskiy <andrey5.1997@mail.ru>
2021-10-08 16:32:48 +0500

    commit(2): fixing

--- js/data.js
+++ js/data.js
@@ -1,41 +1,5 @@
 import {getRandomNumbarValue} from './util.js';
-
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const NAMES = [
-  'Андрей',
-  'Алена',
-  'Ольга',
-  'Лев',
-  'Максим',
-  'Игорь',
-  'Владимир',
-  'Елена',
-  'Рома',
-  'Сергей',
-];
-
-const DESCRIPTION = [
-  'Phasellus quis mauris ac nibh.',
-  'Duis pellentesque tincidunt urna vel.',
-  'Vestibulum ante ipsum primis in.',
-  'Curabitur laoreet tristique luctus. Quisque.',
-  'Integer vitae augue in ligula.',
-  'Quisque aliquam purus ac dui.',
-  'Morbi iaculis imperdiet pretium. Proin.',
-  'Integer ullamcorper, eros in porttitor.',
-  'Donec pulvinar metus est, et.',
-  'Sed ac velit quis neque.',
-];
-
-const NECESSARY_SUM_OBJECTS = 25;
+import {MESSAGES, NAMES, DESCRIPTION} from './const.js';
 
 const generateData = (num, functionGenerate) => {
   const arrayObjects = [];
@@ -72,5 +36,4 @@ const generateObject = (num) => {
   return object;
 };
 
-// Массив нужных данных
-generateData(NECESSARY_SUM_OBJECTS, generateObject);
+export {generateData, generateObject};
--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 import './util.js';
 import './data.js';
+
+import {NECESSARY_SUM_OBJECTS} from './const.js';
+import {generateData, generateObject} from './data.js';
+
+// Массив нужных данных
+const objectsData = generateData(NECESSARY_SUM_OBJECTS, generateObject);
+
+export {objectsData};
