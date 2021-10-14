d0fddd14f700e8982a80100bce56af3c90336d84
Yasnovskiy <andrey5.1997@mail.ru>
2021-10-14 12:44:28 +0500

    commit(3): fixed logic of the application

--- js/picture.js
+++ js/picture.js
@@ -1,21 +1,21 @@
-import {NECESSARY_SUM_OBJECTS} from './const.js';
-import {generateData, generateObject} from './data.js';
-
-const objectsData = generateData(NECESSARY_SUM_OBJECTS, generateObject);
-
 const picturesListElement = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const picturesListFragment = document.createDocumentFragment();
 
-objectsData.forEach(({url, likes, comments}) => {
-  const pictureElement = picturesTemplate.cloneNode(true);
+const generatePictures = (objectsData) => {
 
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  objectsData.forEach(({url, likes, comments}) => {
+    const pictureElement = picturesTemplate.cloneNode(true);
 
-  picturesListFragment.appendChild(pictureElement);
-});
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
 
-picturesListElement.appendChild(picturesListFragment);
+    picturesListFragment.appendChild(pictureElement);
+  });
+
+  return picturesListElement.appendChild(picturesListFragment);
+};
+
+export {generatePictures};
