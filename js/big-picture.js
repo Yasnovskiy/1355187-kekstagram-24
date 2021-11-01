7e75bbb5d9d692679730f302e247006d25e568e2
Yasnovskiy <andrey5.1997@mail.ru>
2021-11-01 17:26:09 +0500

    commit(4): finish fixing

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,30 +1,30 @@
-// Показываю привью большой картинки
-const bigPicture = document.querySelector('.big-picture');
-// Класс убирает скрол с маленьких картинок
 const body = document.body;
-// Класс убирает количество коментариев
+const bigPicture = document.querySelector('.big-picture');
 const commentCount = document.querySelector('.social__comment-count');
 const commentCountView = document.querySelector('.comments-count--view');
-// Класс убирает скрол с маленьких картинок
 const commentsLoader = document.querySelector('.comments-loader');
 
-const initiateOpenBigPicture = () => {
+let handlerLotComments = () => {};
+
+const openBigPicture = () => {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
 };
 
-const initiateCloseBigPicture = () => {
+const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
   commentCount.classList.remove('hidden');
   commentsLoader.classList.remove('hidden');
+
+  commentsLoader.removeEventListener('click', handlerLotComments);
 };
 
 // Для удаления через клавишу ESC
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    initiateCloseBigPicture();
+    closeBigPicture();
   }
 };
 
@@ -66,10 +66,7 @@ const showComments = (array) => {
 
   const arrayComment = createRange(array, 5);
 
-  const {
-    count,
-    range,
-  } = arrayComment();
+  const {count, range} = arrayComment();
   commentsView.textContent = count;
   renderComments(range);
 
@@ -78,25 +75,19 @@ const showComments = (array) => {
     commentsLoader.classList.add('hidden');
   }
 
-  commentsLoader.addEventListener('click', () => {
+  handlerLotComments = () => {
     const {count, range} = arrayComment();
 
     commentsView.textContent = count;
-
-    /*тут как то стронно работает, получается при нажатии на первой фотографии все окей, но когда мы откроем любую другую где комментарий большем чем на первой
-      то при нажати на Загрузить еще то каунт кинет сначало последнее значение 1 фотографии, а через секунду кинет второе значение (которое сейчас есть)
-      про html это пофигу, так как он еще раз изменит, а для проверке нет, она сразу удалят класс....Я не знаю что делать.
-      Сделать тайм аут, или как то проверять несколько раз
-    */
-    console.log(count);
 
     if (array.length === count) {
       commentsLoader.classList.add('hidden');
     }
 
     renderComments(range);
+  };
 
-  });
+  commentsLoader.addEventListener('click', handlerLotComments);
 };
 
 const showBigPictures = (objectsData) => {
@@ -122,12 +113,10 @@ const showBigPictures = (objectsData) => {
   const fdsdfsfsdfasf = document.querySelectorAll('.social__comment');
   commentCountView.textContent = fdsdfsfsdfasf.length;
 
-  initiateOpenBigPicture();
+  openBigPicture();
 
-  //Обработчики закрытия
-  bigPicture.querySelector('.big-picture__cancel').addEventListener('click', initiateCloseBigPicture);
+  bigPicture.querySelector('.big-picture__cancel').addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', onDocumentKeydown);
-
 };
 
 export { showBigPictures };
